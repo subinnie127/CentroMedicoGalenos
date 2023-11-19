f69462ddb9b8fb5eab6a1783e942b2fbb5fa5615
puchito <125156309+CristobalFMV@users.noreply.github.com>
2023-11-19 19:24:21 -0300

    va dentro de la carpeta /centromedico/centromedico
    lo que se subio antes va en /centromedico nomas
    
    no se usar bien esta wea

--- urls.py
+++ urls.py
@@ -1,15 +1,29 @@
-from django.urls import path
-from .views import home, poblar_bd,administrador, secretaria,cajero, medico, paciente, Usuario, calendario
+"""
+URL configuration for TiendaTaller project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-    path('', home, name="home"),
-    path('poblar_bd', poblar_bd, name="poblar_bd"),
-    path('Usuario/<action>/<id>', Usuario, name="Usuario"),
-    path('administrador' ,administrador, name="administrador"),
-    path('secretaria' ,secretaria, name= "secretaria"),
-    path('paciente', paciente, name="paciente"),
-    path('medico',medico, name="medico"),
-    path('cajero', cajero, name="cajero"),
-    path('calendario', calendario, name="calendario")
-
+    path('admin/', admin.site.urls),
+    path('', include('core.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
