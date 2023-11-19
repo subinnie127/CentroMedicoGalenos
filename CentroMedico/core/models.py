add6ba43bcd57191e6716e0f827ccff29be9f43a
subinnie127 <romysynyster5@gmail.com>
2023-11-19 00:17:53 -0300

    bd funciona

--- CentroMedico/core/models.py
+++ CentroMedico/core/models.py
@@ -1,29 +1,19 @@
 from django.db import models
 
-# Create your models here.
-
-class Categoria(models.Model):
-    idCategoria = models.IntegerField(primary_key=True, verbose_name="Id de categoría")
-    nombreCategoria = models.CharField(max_length=80, blank=False, null=False, verbose_name="Nombre de la categoría")
-
-    def __str__(self):
-        return self.nombreCategoria
-
-# Create Modelo para vehículo
-
-class Vehiculo(models.Model):
-    patente = models.CharField(max_length=6, primary_key=True, verbose_name="Patente")
-    marca = models.CharField(max_length=80, blank=False, null=False, verbose_name="Marca vehículo")
-    modelo = models.CharField(max_length=80, null=True, blank=True, verbose_name="Modelo")
-    imagen = models.ImageField(upload_to="images/", default="sinfoto.jpg", null=False, blank=False, verbose_name="Imagen")
-    OPCIONES_SERVICIO = (
-        ('suspension_direccion', 'Suspension y Direccion'),
-        ('cajas_cambio', 'Cajas de Cambio'),
-        ('electronica_automotriz', 'Electronica Automotriz'),
+class Usuario(models.Model):
+    rut = models.CharField(max_length=10, primary_key=True, verbose_name="rut")
+    nombre = models.CharField(max_length=80, blank=False, null=False, verbose_name="nombre")
+    apellido = models.CharField(max_length=80, null=True, blank=True, verbose_name="apellido")
+    TIPO_DE_USUARIO = (
+        ('Secretaria', 'Secretaria'),
+        ('Cajero', 'Cajero'),
+        ('Medico', 'Medico'),
+        ('Paciente', 'Paciente'),
     )
-    servicio = models.CharField(max_length=40, blank=False, null=False, verbose_name="servicio", choices=OPCIONES_SERVICIO)
-    detalle_servicio = models.CharField(max_length=200, blank=False, null=False, verbose_name="detalle servicio")
-    categoria = models.ForeignKey(Categoria, on_delete=models.DO_NOTHING)
+    tipo_de_usuario = models.CharField(max_length=40, 
+                                       blank=False, null=False, 
+                                       verbose_name="tipo de usuario", 
+                                       choices=TIPO_DE_USUARIO)
 
     def __str__(self):
-        return self.patente
+        return self.rut
--- CentroMedico/core/urls.py
+++ CentroMedico/core/urls.py
@@ -1,13 +1,11 @@
 from django.urls import path
-from .views import home, poblar_bd, vehiculo, vehiculo_tienda, vehiculo_ficha,admin, secretaria,cajero, medico, paciente
+from .views import home, poblar_bd,administrador, secretaria,cajero, medico, paciente, Usuario
 
 urlpatterns = [
     path('', home, name="home"),
     path('poblar_bd', poblar_bd, name="poblar_bd"),
-    path('vehiculo/<action>/<id>', vehiculo, name="vehiculo"),
-    path('vehiculo_tienda', vehiculo_tienda, name="vehiculo_tienda"),
-    path('vehiculo_ficha/<id>', vehiculo_ficha, name="vehiculo_ficha"),
-    path('admin' ,admin, name="admin"),
+    path('Usuario/<action>/<id>', Usuario, name="Usuario"),
+    path('administrador' ,administrador, name="administrador"),
     path('secretaria' ,secretaria, name= "secretaria"),
     path('paciente', paciente, name="paciente"),
     path('medico',medico, name="medico"),
