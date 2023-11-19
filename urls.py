b19450318c65211f657ff1c1b3ea7793014d7e95
puchito <125156309+CristobalFMV@users.noreply.github.com>
2023-11-19 19:21:01 -0300

    Add files via upload

---
+++ urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from .views import home, poblar_bd,administrador, secretaria,cajero, medico, paciente, Usuario, calendario
+
+urlpatterns = [
+    path('', home, name="home"),
+    path('poblar_bd', poblar_bd, name="poblar_bd"),
+    path('Usuario/<action>/<id>', Usuario, name="Usuario"),
+    path('administrador' ,administrador, name="administrador"),
+    path('secretaria' ,secretaria, name= "secretaria"),
+    path('paciente', paciente, name="paciente"),
+    path('medico',medico, name="medico"),
+    path('cajero', cajero, name="cajero"),
+    path('calendario', calendario, name="calendario")
+
+]
